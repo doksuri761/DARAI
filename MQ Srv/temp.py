418f320cc015f91e39b8aaa2dfd11288b783c0c9
DESKTOP-C03SHVG\facto <factorio@naver.com>
2023-07-10 23:36:25 +0900

    2a9d97

--- MQ Srv/temp.py
+++ MQ Srv/temp.py
@@ -1,11 +1,32 @@
 import socket
+import timeit
+import threading
+import random
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('127.0.0.1', 8009)
-sock.connect(server_address)
-request = "POST /enqueue/{}?item={}&key={} HTTP/1.1\r\nHost: {}.com\r\n\r\n"
-sock.sendall(request.encode())
-response = sock.recv(4096)
-response_text = response.decode()
-sock.close()
-print(response_text)
+
+def send(a):
+    file = open(f"test{a}.csv", "w")
+    file.write("Conn Time., Send Time, Recv Time\n")
+    t = timeit.default_timer()
+    for _ in range(1000):
+        t1 = timeit.default_timer()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = ('127.0.0.1', 8009)
+        sock.connect(server_address)
+        t2 = timeit.default_timer()
+        request = f"POST /enqueue/OMR?item={None}&key={None} HTTP/1.1\r\nHost: {None}.com\r\n\r\n"
+        sock.sendall(request.encode())
+        t3 = timeit.default_timer()
+        response = sock.recv(4096)
+        response_text = response.decode()
+        t4 = timeit.default_timer()
+        sock.close()
+        file.write(f"{round((t2 - t1) * 1000, 5)}, {round((t3 - t2) * 1000, 5)}, {round((t4 - t3) * 1000, 5)}\n")
+    print(timeit.default_timer() - t, " for 1000 times")
+    file.close()
+
+
+ths = [threading.Thread(target=send, args=[i]) for i in range(10)]
+for th in ths:
+    th.start()
+    th.join()
